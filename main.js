7d96aecfacf84589c98b97061ef40fd790e2c559
Kogan <koganp42@gmail.com>
2019-11-20 11:23:32 -0600

    Working on getting the time display to work, changed code line of moment

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const now = moment();
-console.log(now);
+$(document).ready(function() {
+    let now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    let dateAndTimeDisplay = (momentDisplay, 1000);
+    function momentDisplay() {
+        $("#currentday").text(now);    
+    };
+    console.log(now)
+
+    $(".saveBtn").click(function( event ) {
+        event.preventDefault()
+    });
+    
+    function textStorage() {
+        $()
+    }
+})
