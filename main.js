92a5ed7e1b6e575070527751d4541f78f48a21cb
Kogan <koganp42@gmail.com>
2019-11-20 17:45:41 -0600

    The time-dependent color change works now! Also starting on local store

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ $(document).ready(function() {
     };
     //console.log(now)
 
-    $(".saveBtn").click(function( event ) {
+    $(".saveBtn").click(function( event ){
         event.preventDefault();
-        textStorage();
-    });
-    
-    function textStorage() {
-        $("textarea")
-    }
+        localStorage.setItem($(this).siblings("label").text(), $(this).siblings("textarea").val())
+    }) 
 
+
+
+    console.log(localStorage);
+
+    //These variables correspond to the time slots listed on the schedule, 9-5.
     let nineTime = moment.utc("15:00", "HH:mm");
     let tenTime = moment.utc("16:00", "HH:mm");
     let elevenTime = moment.utc("17:00", "HH:mm");
@@ -24,69 +25,80 @@ $(document).ready(function() {
     let threeTime = moment.utc("21:00", "HH:mm");
     let fourTime = moment.utc("22:00", "HH:mm");
     let fiveTime = moment.utc("23:00", "HH:mm");
+    //Keeping this array here in case I decided to try iterating through it with one if/elseif/else statement.
     //let timeArray = [nineTime, tenTime, elevenTime, twelveTime, oneTime, twoTime, threeTime, fourTime, fiveTime];
-    //console.log(timeArray);
 
-    //The below code section 
-    if (moment().diff(nineTime) > 1) {
+    //The below code section calls the moment function and compares it to the predetermined hour slot variables, changing the styling of the text areas according to whether the hour slot is in the past, present, or future.
+    if (moment().diff(nineTime) > (3.6e+6)) {
         $("#nineTextArea").addClass("past");
+    } else if(moment().diff(nineTime) > 1 && moment().diff(nineTime) < (3.6e+6)) {
+        $("#nineTextArea").addClass("present");
     } else {
         $("#nineTextArea").addClass("future");
     };
 
-    if (moment().diff(tenTime) > 1) {
+    if (moment().diff(tenTime) > (3.6e+6)) {
         $("#tenTextArea").addClass("past");
+    } else if(moment().diff(tenTime) > 1 && moment().diff(tenTime) < (3.6e+6)) {
+        $("#tenTextArea").addClass("present");
     } else {
         $("#tenTextArea").addClass("future");
     };
 
-    if (moment().diff(elevenTime) > 1) {
+    if (moment().diff(elevenTime) > (3.6e+6)) {
         $("#elevenTextArea").addClass("past");
+    } else if(moment().diff(elevenTime) > 1 && moment().diff(elevenTime) < (3.6e+6)) {
+        $("#elevenTextArea").addClass("present");
     } else {
         $("#elevenTextArea").addClass("future");
     };
 
-    if (moment().diff(twelveTime) > 1) {
+    if (moment().diff(twelveTime) > (3.6e+6)) {
+        $("#twelveTextArea").addClass("past");
+    } else if(moment().diff(twelveTime) > 1 && moment().diff(twelveTime) < (3.6e+6)) {
         $("#twelveTextArea").addClass("past");
     } else {
         $("#twelveTextArea").addClass("future");
     };
 
-    if (moment().diff(oneTime) > 1) {
+    if (moment().diff(oneTime) > (3.6e+6)) {
         $("#oneTextArea").addClass("past");
+    } else if(moment().diff(oneTime) > 1 && moment().diff(oneTime) < (3.6e+6)) {
+        $("#oneTextArea").addClass("present");
     } else {
         $("#oneTextArea").addClass("future");
     };
 
-    if (moment().diff(twoTime) > 1) {
+    if (moment().diff(twoTime) > (3.6e+6)) {
         $("#twoTextArea").addClass("past");
+    } else if(moment().diff(twoTime) > 1 && moment().diff(twoTime) < (3.6e+6)) {
+        $("#twoTextArea").addClass("present");
     } else {
         $("#twoTextArea").addClass("future");
     };
 
-    if (moment().diff(threeTime) > 1) {
+    if (moment().diff(threeTime) > (3.6e+6)) {
         $("#threeTextArea").addClass("past");
+    } else if(moment().diff(threeTime) > 1 && moment().diff(threeTime) < (3.6e+6)) {
+        $("#threeTextArea").addClass("present");
     } else {
         $("#threeTextArea").addClass("future");
     };
 
-    if (moment().diff(fourTime) > 1) {
+    if (moment().diff(fourTime) > (3.6e+6)) {
         $("#fourTextArea").addClass("past");
+    } else if(moment().diff(fourTime) > 1 && moment().diff(fourTime) < (3.6e+6)) {
+        $("#fourTextArea").addClass("present");
     } else {
         $("#fourTextArea").addClass("future");
     };
 
-    if (moment().diff(fiveTime) > 1) {
-        $("#fiveTextArea").addClass("past");
+    if (moment().diff(fiveTime) > (3.6e+6)) {
+        $("#fourTextArea").addClass("past");
+    } else if(moment().diff(fiveTime) > 1 && moment().diff(fiveTime) < (3.6e+6)) {
+        $("#fiveTextArea").addClass("present");
     } else {
         $("#fiveTextArea").addClass("future");
     };
     
-    // for (i = 0; i < timeArray.length; i++) {
-    //     if (now.diff(timeArray[i]) > 0) {
-    //         $(".")
-    //     }
-    // }
-
-    //dateAndTimeDisplay;
 })
